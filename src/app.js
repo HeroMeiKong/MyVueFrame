49c6fbf1f679da70663ac82d1042d3a48467f06c
xiaopang <xiaopangfeng4@hotmail.com>
2018-09-16 00:04:58 +0800

    使用chai.js添加测试用例

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import  Vue from 'vue'
-import  Button from './button'
-import  Icon from './icon'
-import  ButtonGroup from './button-group'
+import Vue from 'vue'
+import vm from './vm'
+import Icon from './icon'
+import vmGroup from './vm-group'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
-Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-vm', vm)
+Vue.component('g-icon', Icon)
+Vue.component('g-vm-group', vmGroup)
 new Vue({
     el: '#app',
     data: {
@@ -14,3 +14,83 @@ new Vue({
         loading3: false
     }
 })
+//单元测试
+import chai from 'chai'
+
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set'
+        }
+    })
+    vm.$mount('#test')//vm.$mount()也行
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-set')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set',
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)//order =window.getComputedStyle(svg).order
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)//order =window.getComputedStyle(svg).order
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(vm)
+    const gvm = new Constructor({
+        propsData: {
+            icon: 'set'
+        }
+    })
+    gvm.$mount()
+    gvm.$on('click', function () {
+        console.log(1)
+    })
+    let vm = gvm.$el
+    vm.click()
+    vm.$el.remove()
+    vm.$destroy()
+}
