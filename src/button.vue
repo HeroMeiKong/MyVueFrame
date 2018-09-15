1736f406139964481609b1a36d312eb7b6acdff2
xiaopang <xiaopangfeng4@hotmail.com>
2018-09-15 18:14:58 +0800

    添加icon和icon属性

--- src/button.vue
+++ src/button.vue
@@ -1,25 +1,38 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        props: ['icon', 'iconPosition']
+    }
 </script>
 <style lang="scss">
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 .5em;
         border-radius: var(--border-radius);
-        border:1px solid var(--border-color);
+        border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover { border-color: var(--border-color-hover); }
+        &:active { background-color: var(--button-active-bg); }
+        &:focus { outline: none; }
+        > .icon { order: 1; margin-right: .2em;}
+        > .content { order: 2; }
+        &.icon-right {
+            > .icon { order: 2;  margin-left: .2em;  margin-right: 0;}
+            > .content { order: 1; }
         }
     }
 </style>
