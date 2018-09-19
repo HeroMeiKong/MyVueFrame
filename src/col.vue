11557cb051201691b989210a76e82ac99d9f5e2f
xiaopang <xiaopangfeng4@hotmail.com>
2018-09-19 10:58:52 +0800

    重构row.vue和col.vue

--- src/col.vue
+++ src/col.vue
@@ -1,8 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]" :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
-        <div style="border: 1px solid green;height: 100px;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClasses" :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 <script>
@@ -19,7 +17,17 @@
         },
         data() {
             return {
-                gutter: 0
+                gutter: 0,
+            }
+        },
+        computed: {
+            colClasses(){
+                let {span, offset} = this
+                return [span && `col-${span}`, offset && `offset-${offset}`]
+            },
+            colStyle() {
+                let {gutter} = this
+                return {paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'}
             }
         }
     }
