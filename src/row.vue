5f235b8445acf8fa56b0a0b8d80b334e0fff4a98
xiaopang <xiaopangfeng4@hotmail.com>
2018-09-19 11:35:09 +0800

    添加row的align属性

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="rowStyle">
+    <div class="row" :style="rowStyle" :class="rowClass">
         <slot></slot>
     </div>
 </template>
@@ -8,22 +8,43 @@
         name: 'SoulWalkerRow',
         props: {
             gutter: {
-                type: [Number,String]
+                type: [Number, String]
+            },
+            align: {
+                type: String,
+                validator(value) {
+                    return ['left', 'right', 'center'].includes(value)
+                }
             }
         },
-        mounted(){
-            this.$children.forEach((vm)=>{
+        mounted() {
+            this.$children.forEach((vm) => {
                 vm.gutter = this.gutter
             })
         },
         computed: {
-            rowStyle(){
+            rowStyle() {
                 let {gutter} = this
-                return {marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}
+                return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+            },
+            rowClass() {
+                let {align} = this
+                return [align && `align-${align}`]
             }
         }
     }
 </script>
 <style lang="scss" scoped>
-    .row{ display: flex;}
+    .row {
+        display: flex;
+        &.align-left{
+            justify-content: flex-start;
+        }
+        &.align-right{
+            justify-content: flex-end;
+        }
+        &.align-center{
+            justify-content: center;
+        }
+    }
 </style>
