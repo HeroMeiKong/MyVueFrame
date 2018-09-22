6b688439c15a7508aa5f4ac26ec178e5bf9289a9
xiaopang <xiaopangfeng4@hotmail.com>
2018-09-22 15:31:43 +0800

    完成点击toast关闭

--- src/toast.vue
+++ src/toast.vue
@@ -1,18 +1,59 @@
 <template>
     <div class="toast">
         <slot></slot>
+        <div class="line"></div>
+        <span class="close" v-if="closeButton" @click="onClickClose">
+            {{closeButton.text}}
+        </span>
     </div>
 </template>
 <script>
     export default {
         name: 'SoulWalkerToast',
+        props: {
+            autoClose: {
+                type: Boolean,
+                default: true
+            },
+            autoCloseDelay: {
+                type: Number,
+                default: 30
+            },
+            closeButton: {
+                type: Object,
+                default: () => {
+                    return {
+                        text: '关闭', callback: (toast) => {
+                            toast.close()
+                        }
+                    }
+                }
+            }
+        },
+        mounted() {
+            if (this.autoClose) {
+                setTimeout(() => {
+                    this.close()
+                }, this.autoCloseDelay * 1000)
+            }
+        },
+        methods: {
+            close() {
+                this.$el.remove()
+                this.$destroy()
+            },
+            onClickClose(){
+                this.close()
+                this.closeButton.callback()
+            }
+        }
     }
 </script>
 <style lang="scss" scoped>
     $font-size: 14px;
     $toast-height: 40px;
-    $toast-bg: rgba(0,0,0,.75);
-    .toast{
+    $toast-bg: rgba(0, 0, 0, .75);
+    .toast {
         position: fixed;
         left: 50%;
         top: 0;
@@ -24,8 +65,16 @@
         line-height: 1.8;
         height: $toast-height;
         border-radius: 4px;
-        box-shadow: 0 0 3px 0 rgba(0,0,0,.5);
+        box-shadow: 0 0 3px 0 rgba(0, 0, 0, .5);
         color: white;
         padding: 0 16px;
+    }
+    .close{
+        padding-left: 16px;
+    }
+    .line{
+        height: 100%;
+        border: 1px solid #666;
+        margin-left: 16px;
     }
 </style>
